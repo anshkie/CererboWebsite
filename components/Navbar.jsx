5571c1b893f52025fef82ac597d61009d751473c
Ansh Gupta <202251052@iiitvadodara.ac.in>
2024-10-29 12:00:16 +0530

    Update Navbar.jsx
    updated for contact us

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -101,7 +101,7 @@ const Navbar = () => {
         <div className="hidden md:flex space-x-4 bg-purple-500 rounded-xl px-6 py-6">
           <NavLink href="/sponsors">Sponsors</NavLink>
           <NavLink href="/team">Team</NavLink>
-          <NavLink href="/contactUs">Contact Us</NavLink>
+          <NavLink href="/contact-us">Contact Us</NavLink>
         </div>
 
         {/* Hamburger Menu */}
